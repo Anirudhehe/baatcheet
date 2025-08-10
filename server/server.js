14e9646a92665c24422ddc83761aafdf86219ad3
Anirudh Gupta <anirudhgupta113@gmail.com>
2025-08-10 17:17:00 +0530

    fixed backend deployement bug

--- server/server.js
+++ server/server.js
@@ -50,8 +50,13 @@ app.use("/api/messages",messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV!=="production"){
 
-server.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`);
-})
+    const PORT = process.env.PORT || 5000;
+    
+    server.listen(PORT,()=>{
+        console.log(`server is running on ${PORT}`);
+    })
+}
+// for production
+export default server;
